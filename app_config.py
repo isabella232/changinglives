2e2feaf27fc7fef41d4ede2cea4fdf7b22b5c5ca
Christopher Groskopf <staringmonkey@gmail.com>
2013-04-17 10:45:09 -0500

    Initial import from app-template.

---
+++ app_config.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+"""
+Project-wide application configuration.
+
+DO NOT STORE SECRETS, PASSWORDS, ETC. IN THIS FILE.
+They will be exposed to users. Use environment variables instead.
+"""
+
+import os
+
+PROJECT_NAME = 'App Template'
+PROJECT_SLUG = 'app-template'
+REPOSITORY_NAME = 'app-template'
+CONFIG_NAME = PROJECT_SLUG.replace('-', '').upper()
+
+PROJECT_CREDITS = 'Jeremy Bowers, Brian Boyer, Alyson Hurt and Matt Stiles / NPR'
+PROJECT_SHORTLINK = 'npr.org/foo'
+
+PRODUCTION_S3_BUCKETS = ['apps.npr.org', 'apps2.npr.org']
+PRODUCTION_SERVERS = ['54.244.84.250']
+
+STAGING_S3_BUCKETS = ['stage-apps.npr.org']
+STAGING_SERVERS = ['54.244.169.197']
+
+S3_BUCKETS = []
+SERVERS = []
+DEBUG = True
+
+PROJECT_DESCRIPTION = 'An opinionated project template for client-side apps.'
+SHARE_URL = 'http://%s/%s/' % (PRODUCTION_S3_BUCKETS[0], PROJECT_SLUG)
+
+COPY_GOOGLE_DOC_KEY = '0AlXMOHKxzQVRdHZuX1UycXplRlBfLVB0UVNldHJYZmc'
+
+TWITTER = {
+    'TEXT': PROJECT_NAME,
+    'URL': SHARE_URL
+}
+
+FACEBOOK = {
+    'TITLE': PROJECT_NAME,
+    'URL': SHARE_URL,
+    'DESCRIPTION': PROJECT_DESCRIPTION,
+    'IMAGE_URL': '',
+    'APP_ID': '138837436154588'
+}
+
+NPR_DFP = {
+    'STORY_ID': '171421875',
+    'TARGET': '\/news_politics;storyid=171421875'
+}
+
+GOOGLE_ANALYTICS_ID = 'UA-5828686-4'
+
+TUMBLR_TAGS = 'ford,sass,hoopy,frood,magrathea'
+TUMBLR_FILENAME = 'www/live-data/%s-data.json' % PROJECT_SLUG
+
+# LOG_PATH = '/var/log/%s.log' % PROJECT_SLUG
+LOG_PATH = 'data/test.log'
+
+def get_secrets():
+    """
+    A method for accessing our secrets.
+    """
+    secrets = [
+        '%s_TUMBLR_APP_KEY' % CONFIG_NAME,
+        '%s_TUMBLR_OAUTH_TOKEN' % CONFIG_NAME,
+        '%s_TUMBLR_OAUTH_TOKEN_SECRET' % CONFIG_NAME,
+        '%s_TUMBLR_APP_SECRET' % CONFIG_NAME,
+        'AWS_SECRET_ACCESS_KEY',
+        'AWS_ACCESS_KEY_ID'
+    ]
+    secrets_dict = {}
+    for secret in secrets:
+        # Saves the secret with the old name.
+        secrets_dict[secret.replace('%s_' % CONFIG_NAME, '')] = os.environ.get(secret, None)
+
+    return secrets_dict
+
+def configure_targets(deployment_target):
+    """
+    Configure deployment targets. Abstracted so this can be
+    overriden for rendering before deployment.
+    """
+    global S3_BUCKETS
+    global SERVERS
+    global DEBUG
+    global TUMBLR_URL
+    global TUMBLR_BLOG_ID
+
+    if deployment_target == 'production':
+        S3_BUCKETS = PRODUCTION_S3_BUCKETS
+        SERVERS = PRODUCTION_SERVERS
+        DEBUG = False
+        TUMBLR_URL = '{{ project_slug }}.tumblr.com'
+        TUMBLR_BLOG_ID = '{{ project_slug }}'
+    else:
+        S3_BUCKETS = STAGING_S3_BUCKETS
+        SERVERS = STAGING_SERVERS
+        DEBUG = True
+        TUMBLR_URL = 'staging-{{ project_slug }}.tumblr.com'
+        TUMBLR_BLOG_ID = 'staging-{{ project_slug}}'
+
+DEPLOYMENT_TARGET = os.environ.get('DEPLOYMENT_TARGET', None)
+
+configure_targets(DEPLOYMENT_TARGET)
---
+++ fabfile.py
@@ -0,0 +1,494 @@
+#!/usr/bin/env python
+
+from glob import glob
+import os
+
+from fabric.api import *
+from jinja2 import Template
+
+import app
+import app_config
+from etc import github
+import tumblr_utils
+
+"""
+Base configuration
+"""
+env.deployed_name = app_config.PROJECT_SLUG
+env.repo_name = app_config.REPOSITORY_NAME
+
+env.deploy_to_servers = True
+env.install_crontab = False
+env.deploy_web_services = True
+
+env.repo_url = 'git://github.com/nprapps/%(repo_name)s.git' % env
+env.alt_repo_url = None
+env.user = 'ubuntu'
+env.python = 'python2.7'
+env.path = '/home/%(user)s/apps/%(repo_name)s' % env
+env.repo_path = '%(path)s/repository' % env
+env.virtualenv_path = '%(path)s/virtualenv' % env
+env.forward_agent = True
+
+SERVICES = [
+    ('app', '%(repo_path)s' % env, 'ini'),
+    ('nginx', '/etc/nginx/locations-enabled/', 'conf'),
+    ('uwsgi', '/etc/init/', 'conf'),
+]
+
+"""
+Environments
+"""
+def production():
+    env.settings = 'production'
+    env.s3_buckets = app_config.PRODUCTION_S3_BUCKETS
+    env.hosts = app_config.PRODUCTION_SERVERS
+
+def staging():
+    env.settings = 'staging'
+    env.s3_buckets = app_config.STAGING_S3_BUCKETS
+    env.hosts = app_config.STAGING_SERVERS
+
+"""
+Branches
+"""
+def stable():
+    """
+    Work on stable branch.
+    """
+    env.branch = 'stable'
+
+def master():
+    """
+    Work on development branch.
+    """
+    env.branch = 'master'
+
+def branch(branch_name):
+    """
+    Work on any specified branch.
+    """
+    env.branch = branch_name
+
+"""
+Template-specific functions
+"""
+def less():
+    """
+    Render LESS files to CSS.
+    """
+    for path in glob('less/*.less'):
+        filename = os.path.split(path)[-1]
+        name = os.path.splitext(filename)[0]
+        out_path = 'www/css/%s.less.css' % name
+
+        local('node_modules/.bin/lessc %s %s' % (path, out_path))
+
+def jst():
+    """
+    Render Underscore templates to a JST package.
+    """
+    local('node_modules/.bin/jst --template underscore jst www/js/templates.js')
+
+def app_config_js():
+    """
+    Render app_config.js to file.
+    """
+    from app import _app_config_js
+
+    response = _app_config_js()
+    js = response[0]
+
+    with open('www/js/app_config.js', 'w') as f:
+        f.write(js)
+
+def render():
+    """
+    Render HTML templates and compile assets.
+    """
+    from flask import g
+
+    less()
+    jst()
+
+    # Fake out deployment target
+    app_config.configure_targets(env.get('settings', None))
+
+    app_config_js()
+
+    compiled_includes = []
+
+    for rule in app.app.url_map.iter_rules():
+        rule_string = rule.rule
+        name = rule.endpoint
+
+        if name == 'static' or name.startswith('_'):
+            print 'Skipping %s' % name
+            continue
+
+        if rule_string.endswith('/'):
+            filename = 'www' + rule_string + 'index.html'
+        elif rule_string.endswith('.html'):
+            filename = 'www' + rule_string
+        else:
+            print 'Skipping %s' % name
+            continue
+
+        dirname = os.path.dirname(filename)
+
+        if not (os.path.exists(dirname)):
+            os.makedirs(dirname)
+
+        print 'Rendering %s' % (filename)
+
+        with app.app.test_request_context(path=rule_string):
+            g.compile_includes = True
+            g.compiled_includes = compiled_includes
+
+            view = app.__dict__[name]
+            content = view()
+
+            compiled_includes = g.compiled_includes
+
+        with open(filename, 'w') as f:
+            f.write(content)
+
+    # Un-fake-out deployment target
+    app_config.configure_targets(app_config.DEPLOYMENT_TARGET)
+
+def tests():
+    """
+    Run Python unit tests.
+    """
+    local('nosetests')
+
+"""
+Setup
+"""
+def setup():
+    """
+    Setup servers for deployment.
+    """
+    require('settings', provided_by=[production, staging])
+    require('branch', provided_by=[stable, master, branch])
+
+    setup_directories()
+    setup_virtualenv()
+    clone_repo()
+    checkout_latest()
+    install_requirements()
+    if env.get('deploy_web_services', False):
+        deploy_confs()
+
+def setup_directories():
+    """
+    Create server directories.
+    """
+    require('settings', provided_by=[production, staging])
+
+    run('mkdir -p %(path)s' % env)
+    sudo('chmod -R 777 /var/www/uploads')
+    run('mkdir -p /var/www/uploads/%s' % app_config.PROJECT_SLUG)
+
+def setup_virtualenv():
+    """
+    Setup a server virtualenv.
+    """
+    require('settings', provided_by=[production, staging])
+
+    run('virtualenv -p %(python)s --no-site-packages %(virtualenv_path)s' % env)
+    run('source %(virtualenv_path)s/bin/activate' % env)
+
+def clone_repo():
+    """
+    Clone the source repository.
+    """
+    require('settings', provided_by=[production, staging])
+
+    run('git clone %(repo_url)s %(repo_path)s' % env)
+
+    if env.get('alt_repo_url', None):
+        run('git remote add bitbucket %(alt_repo_url)s' % env)
+
+def checkout_latest(remote='origin'):
+    """
+    Checkout the latest source.
+    """
+    require('settings', provided_by=[production, staging])
+    require('branch', provided_by=[stable, master, branch])
+
+    env.remote = remote
+
+    run('cd %(repo_path)s; git fetch %(remote)s' % env)
+    run('cd %(repo_path)s; git checkout %(branch)s; git pull %(remote)s %(branch)s' % env)
+
+def install_requirements():
+    """
+    Install the latest requirements.
+    """
+    require('settings', provided_by=[production, staging])
+
+    run('%(virtualenv_path)s/bin/pip install -U -r %(repo_path)s/requirements.txt' % env)
+
+def install_crontab():
+    """
+    Install cron jobs script into cron.d.
+    """
+    require('settings', provided_by=[production, staging])
+
+    sudo('cp %(repo_path)s/crontab /etc/cron.d/%(deployed_name)s' % env)
+
+def uninstall_crontab():
+    """
+    Remove a previously install cron jobs script from cron.d
+    """
+    require('settings', provided_by=[production, staging])
+
+    sudo('rm /etc/cron.d/%(deployed_name)s' % env)
+
+def bootstrap_issues():
+    """
+    Bootstraps Github issues with default configuration.
+    """
+    auth = github.get_auth()
+    github.delete_existing_labels(auth)
+    github.create_default_labels(auth)
+    github.create_default_tickets(auth)
+
+def create_log_file():
+    """
+    Creates the log file for recording Tumblr POSTs.
+    """
+    sudo('touch /var/log/%s.log' % app_config.PROJECT_SLUG)
+    sudo('chown ubuntu /var/log/%s.log' % app_config.PROJECT_SLUG)
+
+def install_scout_plugins():
+    """
+    Install plugins to Scout.
+    """
+    with settings(warn_only=True):
+        run('ln -s %(repo_path)s/scout/*.rb ~/.scout' % env)
+
+def generate_new_oauth_tokens():
+    tumblr_utils.generate_new_oauth_tokens()
+
+"""
+Deployment
+"""
+def _deploy_to_s3():
+    """
+    Deploy the gzipped stuff to
+    """
+    s3cmd = 's3cmd -P --add-header=Cache-Control:max-age=5 --guess-mime-type --recursive --exclude-from gzip_types.txt sync gzip/ %s'
+    s3cmd_gzip = 's3cmd -P --add-header=Cache-Control:max-age=5 --add-header=Content-encoding:gzip --guess-mime-type --recursive --exclude "*" --include-from gzip_types.txt sync gzip/ %s'
+
+    for bucket in env.s3_buckets:
+        env.s3_bucket = bucket
+        local(s3cmd % ('s3://%(s3_bucket)s/%(deployed_name)s/' % env))
+        local(s3cmd_gzip % ('s3://%(s3_bucket)s/%(deployed_name)s/' % env))
+
+def _gzip_www():
+    """
+    Gzips everything in www and puts it all in gzip
+    """
+    local('python gzip_www.py')
+    local('rm -rf gzip/live-data')
+
+def _render_theme():
+    """
+    Renders tumblr theme file.
+    """
+    context = {}
+
+    for TEMPLATE in ['_form.html', '_prompt.html', '_social.html']:
+        with open('templates/%s' % TEMPLATE, 'rb') as read_template:
+            payload = Template(read_template.read())
+            payload = payload.render({'SERVERS': env.hosts})
+            parsed_path = TEMPLATE.split('_')[1].split('.')
+            context['%s_%s' % (parsed_path[0].upper(), parsed_path[1].upper())] = payload
+
+    for config in ['NAME', 'CREDITS', 'SHORTLINK']:
+        config = 'PROJECT_%s' % config
+        context[config] = getattr(app_config, config)
+
+    context['STATIC_URL'] = 'http://127.0.0.1:8000/'
+    context['STATIC_CSS'] = '%sless/tumblr.less' % context['STATIC_URL']
+    print env.settings
+    if env.settings == 'production':
+        context['STATIC_URL'] = 'http://%s/%s/' % (env.s3_buckets[0], env.deployed_name)
+        context['STATIC_CSS'] = '%scss/tumblr.less.css' % context['STATIC_URL']
+
+
+    with open('tumblr/theme.html', 'rb') as read_template:
+        payload = Template(read_template.read())
+        return payload.render(**context)
+
+def write_theme():
+    require('settings', provided_by=[production, staging])
+
+    with open('tumblr/rendered-theme.html', 'wb') as write_template:
+        write_template.write(_render_theme())
+
+def copy_theme():
+    require('settings', provided_by=[production, staging])
+
+    write_theme()
+    local('pbcopy < tumblr/rendered-theme.html')
+
+def render_confs():
+    """
+    Renders server configurations.
+    """
+    require('settings', provided_by=[production, staging])
+
+    with settings(warn_only=True):
+        local('mkdir confs/rendered')
+
+    context = app_config.get_secrets()
+    context['PROJECT_SLUG'] = app_config.PROJECT_SLUG
+    context['PROJECT_NAME'] = app_config.PROJECT_NAME
+    context['REPOSITORY_NAME'] = app_config.REPOSITORY_NAME
+    context['DEPLOYMENT_TARGET'] = env.settings
+
+    for service, remote_path, extension in SERVICES:
+        file_path = 'confs/rendered/%s.%s.%s' % (app_config.PROJECT_SLUG, service, extension)
+
+        with open('confs/%s.%s' % (service, extension),  'r') as read_template:
+
+            with open(file_path, 'wb') as write_template:
+                payload = Template(read_template.read())
+                write_template.write(payload.render(**context))
+
+def deploy_confs():
+    """
+    Deploys rendered server configurations to the specified server.
+    This will reload nginx and the appropriate uwsgi config.
+    """
+    require('settings', provided_by=[production, staging])
+
+    render_confs()
+
+    with settings(warn_only=True):
+        run('touch /tmp/%s.sock' % app_config.PROJECT_SLUG)
+        sudo('chmod 777 /tmp/%s.sock' % app_config.PROJECT_SLUG)
+
+        for service, remote_path, extension in SERVICES:
+            service_name = '%s.%s' % (app_config.PROJECT_SLUG, service)
+            file_name = '%s.%s' % (service_name, extension)
+            local_path = 'confs/rendered/%s' % file_name
+            put(local_path, remote_path, use_sudo=True)
+
+            if service == 'nginx':
+                sudo('service nginx reload')
+
+            if service == 'uwsgi':
+                sudo('initctl reload-configuration')
+                sudo('service %s restart' % service_name)
+
+def deploy(remote='origin'):
+    """
+    Deploy the latest app to S3 and, if configured, to our servers.
+    """
+    require('settings', provided_by=[production, staging])
+
+    if env.get('deploy_to_servers', False):
+        require('branch', provided_by=[stable, master, branch])
+
+    if (env.settings == 'production' and env.branch != 'stable'):
+        _confirm("You are trying to deploy the '%(branch)s' branch to production.\nYou should really only deploy a stable branch.\nDo you know what you're doing?" % env)
+
+    render()
+    _gzip_www()
+    _deploy_to_s3()
+
+    if env.get('deploy_to_servers', False):
+        checkout_latest(remote)
+
+        if env.get('install_crontab', False):
+            install_crontab()
+
+def write_json_data():
+    """
+    Writes JSON file to www/live-data/.
+    """
+    tumblr_utils.write_json_data()
+
+def deploy_json_data():
+    """
+    Deploys JSON file to S3.
+    """
+    write_json_data()
+    tumblr_utils.deploy_json_data(env.s3_buckets)
+
+"""
+Cron jobs
+"""
+def cron_test():
+    """
+    Example cron task. Note we use "local" instead of "run"
+    because this will run on the server.
+    """
+    require('settings', provided_by=[production, staging])
+
+    local('echo $DEPLOYMENT_TARGET > /tmp/cron_test.txt')
+
+"""
+Destruction
+"""
+def _confirm(message):
+    answer = prompt(message, default="Not at all")
+
+    if answer.lower() not in ('y', 'yes', 'buzz off', 'screw you'):
+        exit()
+
+def shiva_the_destroyer():
+    """
+    Deletes the app from s3
+    """
+    require('settings', provided_by=[production, staging])
+
+    _confirm("You are about to destroy everything deployed to %(settings)s for this project.\nDo you know what you're doing?" % env)
+
+    with settings(warn_only=True):
+        s3cmd = 's3cmd del --recursive %s'
+
+        for bucket in env.s3_buckets:
+            env.s3_bucket = bucket
+            local(s3cmd % ('s3://%(s3_bucket)s/%(deployed_name)s' % env))
+
+        if env.get('deploy_to_servers', False):
+            run('rm -rf %(path)s' % env)
+
+            if env.get('install_crontab', False):
+                uninstall_crontab()
+
+"""
+Logging
+"""
+def parse_log_to_json():
+    tumblr_utils.parse_log_to_json()
+
+def parse_log_last_24():
+    tumblr_utils.parse_log_last_24()
+
+"""
+App-template meta-commands
+"""
+
+def super_merge():
+    """
+    Merge master branch into all init- branches.
+    """
+    _confirm("You are about to merge 'master' into all 'init-' branches.\nDo you know what you're doing?")
+
+    local('git fetch')
+    local('git checkout master')
+
+    for branch in ['table', 'map', 'chat']:
+        local('git checkout init-%s' % branch)
+        local('git merge origin/init-%s --no-edit' % branch)
+        local('git merge master --no-edit')
+
+    local('git checkout master')
+
+    local('git push --all')
