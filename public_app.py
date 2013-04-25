4707770f2dfe5484cd910d3054004cd23ee3b95e
Christopher Groskopf <staringmonkey@gmail.com>
2013-04-25 13:33:44 -0400

    Perms 2.

--- public_app.py
+++ public_app.py
@@ -72,8 +72,7 @@ def _post_to_tumblr():
     with open('/var/www%s' % svg_path, 'wb') as f:
         f.write(svg.encode('utf-8'))
 
-    os.chmod('/var/www%s' % svg_path, 777)
-
+    envoy.run('chmod 777 /var/www%s' % svg_path)
     envoy.run('cairosvg /var/www%s -f png -o /var/www%s' % (svg_path, png_path))
 
     context = {
