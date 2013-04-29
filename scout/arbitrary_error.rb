2c700d54c6c0cc40be903544021cf5da0c42d86b
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-04-29 09:39:30 -0400

    Updates test log. Updates public_app with some documentation. Adds a scout plugin for arbitrary errors.

--- scout/arbitrary_error.rb
+++ scout/arbitrary_error.rb
@@ -1,35 +1,53 @@
-require 'date'
-
-class ArbitraryErrors < Scout::Plugin
+class PostErrors < Scout::Plugin
   def build_report
 
-    log_file_path = '/var/log/changing-lives.log'
-    count = 0
+    # log_file_path = '/var/log/changing-lives.log'
+    log_file_path = 'data/test.log'
 
-    # Get today's date.
-    today = DateTime.now
+    # First, decide if we're running for the first time or not.
+    if not memory(:count)
 
-    # Now, add four hours to that date.
-    # This also sucks.
-    today = today - Rational(4, 24)
+      # Check the Scout memory for a count. If it's null, we're new!
+      # Just remember the current count, 0, and do nothing else.
+      remember :count => 0
 
-    today = DateTime.parse(today.to_s.split('T')[0])
+    else
 
-    File.open(log_file_path) do |file|
-      file.each do |line|
+      # We are not running for the first time. Let's zero out the count.
+      count = 0
 
-        this_date = DateTime.parse(line.split(' ')[0])
+      # Now, let's read the file for error lines and count them.
+      File.open(log_file_path) do |file|
+        file.each do |line|
 
-        if this_date == today
           if line.include?("ERROR")
             count += 1
           end
+
         end
+      end
+
+      # If the current count of errors in the log is greater than the
+      # remembered number of errors, WE HAVE NEW ERRORS and we should
+      # alert Brian because he likes that sort of thing.
+      if count > memory(:count)
+
+        # Remember the current count, blowing away the old count.
+        remember :count => count
+
+        # Raise an alert. First bit is the subject, second is the body.
+        $count = count
+        alert('Errors in Changing Lives!', 'There have been #$count errors since the last report.')
+
+      else
+
+        # Otherwise, just remember the current count by looking up the
+        # one in memory now and saving it.
+        remember :count => count
 
       end
-    end
 
-    report(:errors => count)
+    end
 
   end
 end
