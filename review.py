a477a59f594c6baf1d2d48e325981d63a6cfbfdc
Christopher Groskopf <staringmonkey@gmail.com>
2013-05-08 14:16:43 -0500

    Review sort of working.

--- review.py
+++ review.py
@@ -2,6 +2,7 @@
 
 import csv
 from glob import glob
+import os
 import re
 
 from flask import Flask, render_template
@@ -19,34 +20,38 @@ unmapped_svg_urls = list()
 mapped_tumblr_urls = list()
 unmapped_tumblr_urls = list()
 
+bad_tumblr_urls = list()
+bad_svg_urls = list()
+
 next_tumblr = 0
 
 def setup():
-    with open('data/review.csv') as f:
-        reader = csv.DictReader(f, fieldnames=CSV_HEADERS)
+    if os.path.exists('data/review.csv'):
+        with open('data/review.csv') as f:
+            reader = csv.DictReader(f, fieldnames=CSV_HEADERS)
 
-        for row in reader:
-            svg_map[row['svg_url']] = (row['status'], row['tumblr_url'])
+            for row in reader:
+                svg_map[row['svg_url']] = (row['status'], row['tumblr_url'])
 
-            if row['tumblr_url']:
-                mapped_svg_urls.append(row['svg_url'])
-                mapped_tumblr_urls.append(row['tumblr_url'])
-            else:
-                unmapped_svg_urls.append(row['svg_url'])
+                if row['tumblr_url']:
+                    mapped_svg_urls.append(row['svg_url'])
+                    mapped_tumblr_urls.append(row['tumblr_url'])
+                else:
+                    unmapped_svg_urls.append(row['svg_url'])
 
-    for svg_path in glob('/var/www/uploads/%s/*.svg' % app_config.PROJECT_SLUG):
+    for svg_path in sorted(glob('/var/www/uploads/%s/*.svg' % app_config.PROJECT_SLUG)):
         svg_url = svg_path.replace('/var/www', 'http://%s' % app_config.SERVERS[0])
 
         if svg_url in mapped_svg_urls:
             pass
-        elif svg_url in  unmapped_svg_urls:
+        elif svg_url in unmapped_svg_urls:
             pass
         else:
             svg_map[svg_url] = (False, '')
             unmapped_svg_urls.append(svg_url)
 
     with open(app_config.LOG_PATH) as f:
-        log = list(f)
+        log = [l.strip() for l in f]
 
     for line in log:
         match = re.search('200 (http.*) reader', line)
@@ -70,7 +75,35 @@ def setup():
 
         tumblr_url = match.group(1)
 
-        unmapped_tumblr_urls.append(tumblr_url)
+	if tumblr_url not in mapped_tumblr_urls:
+            unmapped_tumblr_urls.append(tumblr_url)
+
+    global bad_tumblr_urls
+
+    if os.path.exists('data/bad_tumblr_urls.csv'):
+        with open('data/bad_tumblr_urls.csv') as f:
+            bad_tumblr_urls = [l.strip() for l in f]	
+        
+	for url in bad_tumblr_urls:
+            if url in unmapped_tumblr_urls:
+                unmapped_tumblr_urls.remove(url)
+
+    global bad_svg_urls
+
+    if os.path.exists('data/bad_svg_urls.csv'):
+        with open('data/bad_svg_urls.csv') as f:
+            bad_svg_urls = [l.strip() for l in f]
+
+        for url in bad_svg_urls:
+            if url in unmapped_svg_urls:
+                unmapped_svg_urls.remove(url)
+
+    print 'Mapped SVG urls: %i' % len(mapped_svg_urls)
+    print 'Unmapped SVG urls: %i' % len(unmapped_svg_urls)
+    print 'Mapped Tumblr urls: %i' % len(mapped_tumblr_urls)
+    print 'Unmapped Tumblr urls: %i' % len(unmapped_tumblr_urls)
+    print 'Bad Tumblr urls: %i' % len(bad_tumblr_urls)
+    print 'Bad SVG urls: %i' % len(bad_svg_urls)
 
 def write_csv():
     with open('data/review.csv', 'w') as f:
@@ -81,7 +114,19 @@ def write_csv():
 
         for svg_url in unmapped_svg_urls:
             writer.writerow([svg_url, False, ''])
+
+def write_bad_tumblr_urls():
+    global bad_tumblr_urls
+
+    with open('data/bad_tumblr_urls.csv', 'w') as f:
+        f.write('\n'.join(bad_tumblr_urls))
             
+def write_bad_svg_urls():
+    global bad_svg_urls
+
+    with open('data/bad_svg_urls.csv', 'w') as f:
+        f.write('\n'.join(bad_svg_urls))
+ 
 @app.route('/')
 def review():
     from flask import request
@@ -102,6 +147,14 @@ def review():
         next_tumblr = 0
     elif request.args.get('next', None):
         next_tumblr += 1
+    elif request.args.get('bad_tumblr', None):
+        bad_tumblr_urls.append(unmapped_tumblr_urls.pop(next_tumblr))
+        write_bad_tumblr_urls()
+    elif request.args.get('bad_svg', None):
+        bad_svg_urls.append(unmapped_svg_urls.pop(0))
+        write_bad_svg_urls()
+
+        next_tumblr = 0
 
     tumblr_url = unmapped_tumblr_urls[next_tumblr]
     svg_url = unmapped_svg_urls[0]
@@ -114,4 +167,4 @@ def review():
 
 if __name__ == '__main__':
     setup()
-    app.run(host='0.0.0.0', port=8002, debug=app_config.DEBUG)
+    app.run(host='0.0.0.0', port=8002, debug=True)
