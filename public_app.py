77755bad0174421aa376a37cd275da06dcf07f15
Christopher Groskopf <staringmonkey@gmail.com>
2013-05-09 15:32:30 -0500

    Refactor zazzler into its own module.

--- public_app.py
+++ public_app.py
@@ -9,15 +9,13 @@ import subprocess
 import time
 
 from flask import Flask, redirect, render_template
-import Image
-import ImageDraw
-import ImageFont
 from jinja2.filters import do_mark_safe
 from tumblpy import Tumblpy
 from tumblpy import TumblpyError
 from werkzeug import secure_filename
 
 import app_config
+from zazzle import zazzlify_png
 
 app = Flask(app_config.PROJECT_NAME)
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -185,52 +183,6 @@ def _post_to_tumblr():
         return render_template('500.html', **context)
 
     return redirect('%s#posts' % tumblr_url, code=301)
-
-
-def zazzlify_png(png_path, tumblr_id, name, location):
-    """
-    Add a footer and border to the PNG for Zazzle.
-    """
-    path, filename = os.path.split(png_path)
-    zazzle_path = '%s/%s.png' % (path, tumblr_id)
-
-    border = 128
-    size = 2048
-    text_color = (120, 120, 120)
-    logo_height = 92
-    logo_width = 274
-    disclaimer = 'This design was created for the project She Works: Note To Self and printed in support of NPR. Create your own at: npr.org/sheworks'
-
-    png = Image.open('/var/www/%s' % png_path)
-    zazzle_png = Image.new('RGBA', (size + border * 2, size + border * 2), (0, 0, 0, 0))
-    zazzle_png.paste(png, (border, border))
-
-    draw = ImageDraw.Draw(zazzle_png)
-    font_big = ImageFont.truetype('Knockout-29.otf', 50)
-    font_small = ImageFont.truetype('Knockout-29.otf', 40)
-
-    if name and location:
-        attribution = 'By %s, %s' % (name, location)
-    elif name:
-        attribution = 'By %s' % name
-    elif location:
-        attribution = 'By Anonymous, %s' % location
-    else:
-        attribution = 'By Anonymous'
-
-    attribution = attribution.upper()
-
-    draw.text((border, border + size + 15), attribution, text_color, font=font_big)
-    draw.text((border, border + size + 70), disclaimer, text_color, font=font_small)
-
-    logo = Image.open('www/img/nprlogo-transparent.png')
-    zazzle_png.paste(logo, (size + border - logo_width + 13, size + border + (border - logo_height) / 2))
-
-    if app_config.DEPLOYMENT_TARGET == 'development':
-        zazzle_png.show()
-        print '/var/www/%s' % zazzle_path
-
-    zazzle_png.save('/var/www/%s' % zazzle_path)
 
 
 if __name__ == '__main__':
--- zazzlify_old_pngs.py
+++ zazzlify_old_pngs.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python
 
 import csv
+import os
 
 import app_config
-from public_app import zazzlify_png
+from zazzle import zazzlify_png
 
 with open('data/review_plus.csv') as f:
     rows = list(csv.reader(f))
 
-for row in rows:
+for i, row in enumerate(rows):
     svg_url, status, tumblr_url, name, location = row
+
+    if not tumblr_url:
+        continue
 
     tumblr_id = tumblr_url.split('/')[-1] 
 
     svg_path = svg_url.replace('http://%s/' % app_config.SERVERS[0], '')
     png_path = svg_path.replace('svg', 'png')
+    
+    path, filename = os.path.split(png_path)
+    zazzle_path = os.path.join(path, tumblr_id + '.png')
 
-    zazzlify_png(png_path, tumblr_id, name, location)
+    print i, png_path
+    print zazzle_path
+
+    if os.path.exists('/var/www/' + zazzle_path):
+        print 'Skipping'
+        continue
+
+    print zazzlify_png(png_path, tumblr_id, name, location)
