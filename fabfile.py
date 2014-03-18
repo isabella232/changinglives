1cc90d47feb3ad14cd11766fc1589b4f735b6179
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-03-18 13:19:08 -0400

    lessc and jst are in different places. Ah, node.

--- fabfile.py
+++ fabfile.py
@@ -87,13 +87,19 @@ def less():
         name = os.path.splitext(filename)[0]
         out_path = 'www/css/%s.less.css' % name
 
-        local('node_modules/.bin/lessc %s %s' % (path, out_path))
+        if os.path.exists('node_modules/.bin/lessc'):
+            local('node_modules/.bin/lessc %s %s' % (path, out_path))
+        else:
+            local('node_modules/bin/lessc %s %s' % (path, out_path))
 
 def jst():
     """
     Render Underscore templates to a JST package.
     """
-    local('node_modules/.bin/jst --template underscore jst www/js/templates.js')
+    if os.path.exists('node_modules/.bin/jst'):
+        local('node_modules/.bin/jst --template underscore jst www/js/templates.js')
+    else:
+        local('node_modules/bin/jst --template underscore jst www/js/templates.js')
 
 def download_copy():
     """
